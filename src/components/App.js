d864c62fedea870a87a174dff8a57561cafb2468
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-14 18:18:27 +0530

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
+const INITIAL_STATE = ["Dinesh", "Ramesh", "Suresh"];
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+              
+      <ol key="relativeList" id="relativeList">
+        {INITIAL_STATE?.map((relative, idx) => {
+          return (
+            <li
+              key={"relativeListItem" + (parseInt(idx) + 1)}
+              id={"relativeListItem" + (parseInt(idx) + 1)}
+            >
+              {relative}
+            </li>
+          );
+        })}
+      </ol>
     </div>
+
+
+   
   )
 }
 
